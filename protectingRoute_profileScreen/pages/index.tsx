5bb1e062d0e63fb00cfab5a2f74e43859ae97395
Mbilal6799 <ftk4321@gmail.com>
2023-04-07 11:53:23 -0700

    profileScreen is complete

--- protectingRoute_profileScreen/pages/index.tsx
+++ protectingRoute_profileScreen/pages/index.tsx
@@ -1,13 +1,33 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession,signOut } from "next-auth/react";
+
+export async function getServerSideProps(context:NextPageContext){
+
+  const session=await getSession(context);
+  if(!session){
+    return {
+      redirect:{
+        destination:'/auth',
+        permanent:false,
+      }
+    }
+  }
+
+  return {
+    props:{}
+  }
+
+}
 
 
 export default function Home() {
+  const {data:user} =useCurrentUser();
   return (
     <>
       <h1 className='text-2xl text-green-500'>NetFlix Clone</h1>
+      <p className="text-white">Logged in as : {user?.email}</p>
+      <button onClick={()=>signOut()} className='h-10 w-full bg-white '>Log Out!</button>
     </>
   )
 }
