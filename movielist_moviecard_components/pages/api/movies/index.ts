74492191570b0890fa2dc7b6777d0e88eee6648a
Mbilal6799 <ftk4321@gmail.com>
2023-04-18 15:18:52 -0700

    completed

---
+++ movielist_moviecard_components/pages/api/movies/index.ts
@@ -0,0 +1,24 @@
+import { NextApiResponse,NextApiRequest } from "next";
+
+import prismadb from '@/libs/prismadb';
+import serverAuth from "@/libs/serverAuth";
+
+
+export default async function handler(req:NextApiRequest,res:NextApiResponse) {
+    if(req.method !== "GET"){
+        res.status(405).end();
+    }
+    
+
+    try{
+        await serverAuth(req);
+        const movies=await prismadb.movie.findMany();
+
+        res.status(200).json(movies)
+    }
+    catch(e){
+        console.log(e);
+        res.status(400).end();
+    }
+
+}
